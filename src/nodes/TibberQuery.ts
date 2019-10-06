1fe9b5fe9e4d2fb5b654c40c9fbe30e5de2213aa
André Biseth <andre@biseth.net>
2019-10-06 19:50:53 +0200

    Did some null and type checking on query results

--- src/nodes/TibberQuery.ts
+++ src/nodes/TibberQuery.ts
@@ -32,72 +32,81 @@ export class TibberQuery {
 
     public async getHomes(): Promise<IHome[]> {
         const result = await this.query(gqlHomes);
-        return Object.assign([] as IHome[], result && result.viewer ? result.viewer.homes : result);
+        if (result && result.viewer && Array.isArray(result.viewer.homes)) {
+            return Object.assign([] as IHome[], result.viewer.homes);
+        }
+        return result && result.error ? result : {};
     }
 
     public async getHomesComplete(): Promise<IHome[]> {
         const result = await this.query(gqlHomesComplete);
-        return Object.assign([] as IHome[], result && result.viewer ? result.viewer.homes : result);
+        if (result && result.viewer && Array.isArray(result.viewer.homes)) {
+            return Object.assign([] as IHome[], result.viewer.homes);
+        }
+        return result && result.error ? result : {};
     }
 
     public async getCurrentEnergyPrice(homeId: string): Promise<IPrice> {
         const result = await this.query(gqlCurrentEnergyPrice);
-        if (!result || !result.viewer){
-            return result;
+        if (result && result.viewer && Array.isArray(result.viewer.homes)) {
+            const data: IHome = result.viewer.homes.filter((element: IHome) => element.id === homeId)[0];
+            return Object.assign(
+                {} as IPrice,
+                data && data.currentSubscription && data.currentSubscription.priceInfo ? data.currentSubscription.priceInfo.current : {},
+            );
         }
-        const data: IHome = result.viewer.homes.filter((element: IHome) => element.id === homeId)[0];
-        return Object.assign(
-            {} as IPrice,
-            data && data.currentSubscription && data.currentSubscription.priceInfo ? data.currentSubscription.priceInfo.current : {},
-        );
+        return result && result.error ? result : {};
     }
 
     public async getTodaysEnergyPrices(homeId: string): Promise<IPrice[]> {
         const result = await this.query(gqlTodaysEnergyPrices);
-        if (!result || !result.viewer){
-            return result;
+        if (result && result.viewer && Array.isArray(result.viewer.homes)) {
+            const data: IHome = result.viewer.homes.filter((element: IHome) => element.id === homeId)[0];
+            return Object.assign(
+                [] as IPrice[],
+                data && data.currentSubscription && data.currentSubscription.priceInfo ? data.currentSubscription.priceInfo.today : {},
+            );
         }
-        const data: IHome = result.viewer.homes.filter((element: IHome) => element.id === homeId)[0];
-        return Object.assign(
-            [] as IPrice[],
-            data && data.currentSubscription && data.currentSubscription.priceInfo ? data.currentSubscription.priceInfo.today : {},
-        );
+        return result && result.error ? result : {};
     }
 
     public async getTomorrowsEnergyPrices(homeId: string): Promise<IPrice[]> {
         const result = await this.query(gqlTomorrowsEnergyPrices);
-        if (!result || !result.viewer){
-            return result;
+        if (result && result.viewer && Array.isArray(result.viewer.homes)) {
+            const data: IHome = result.viewer.homes.filter((element: IHome) => element.id === homeId)[0];
+            return Object.assign(
+                [] as IPrice[],
+                data && data.currentSubscription && data.currentSubscription.priceInfo ? data.currentSubscription.priceInfo.tomorrow : {},
+            );
         }
-        const data: IHome = result.viewer.homes.filter((element: IHome) => element.id === homeId)[0];
-        return Object.assign(
-            [] as IPrice[],
-            data && data.currentSubscription && data.currentSubscription.priceInfo ? data.currentSubscription.priceInfo.tomorrow : {},
-        );
+        return result && result.error ? result : {};
     }
 
     public async getCurrentEnergyPrices(): Promise<IHome[]> {
         const result = await this.query(gqlCurrentEnergyPrice);
-        return Object.assign([] as IHome[], result && result.viewer ? result.viewer.homes : result);
+        if (result && result.viewer && Array.isArray(result.viewer.homes)) {
+            return Object.assign([] as IHome[], result.viewer.homes);
+        }
+        return result && result.error ? result : {};
     }
 
     public async getConsumption(resolution: EnergyResolution, lastCount: number, homeId?: string): Promise<IConsumption[]> {
         const variables = { resolution, lastCount };
         const result = await this.query(gqlConsumption, variables);
-        if (!result || !result.viewer){
-            return result;
-        }
-        if(!homeId) {
-            const consumptions = result.viewer.homes.map((item: IHome) => {
-                const nodes = item.consumption.nodes.map((node: IConsumption) => {
-                    node.homeId = item.id;
-                    return node;
+        if (result && result.viewer && Array.isArray(result.viewer.homes)) {
+            if (!homeId) {
+                const consumptions = result.viewer.homes.map((item: IHome) => {
+                    const nodes = item.consumption.nodes.map((node: IConsumption) => {
+                        node.homeId = item.id;
+                        return node;
+                    });
+                    return nodes;
                 });
-                return nodes;
-              });
-            return Object.assign([] as IConsumption[], consumptions);
+                return Object.assign([] as IConsumption[], consumptions);
+            }
+            const home: IHome = result.viewer.homes.filter((element: IHome) => element.id === homeId)[0];
+            return Object.assign([] as IConsumption[], home && home.consumption ? home.consumption.nodes : []);
         }
-        const home: IHome = result.viewer.homes.filter((element: IHome) => element.id === homeId)[0];
-        return Object.assign([] as IConsumption[], home && home.consumption ? home.consumption.nodes : []);
+        return result && result.error ? result : {};
     }
 }
