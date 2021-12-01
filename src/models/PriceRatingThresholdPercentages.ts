5ec8b4fce44c023900ae1462665dd76e5fe70acf
André Biseth <andre@biseth.net>
2021-12-01 08:11:08 +0100

    Added models for priceRating

---
+++ src/models/PriceRatingThresholdPercentages.ts
@@ -0,0 +1,7 @@
+
+export interface PriceRatingThresholdPercentages {
+    /** The percentage difference when the price is considered to be ‘high’ (market dependent) */
+    high: number;
+    /** The percentage difference when the price is considered to be ‘low’ (market dependent) */
+    low: number;
+}
