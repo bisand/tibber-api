5d44207fca6366fa50d8d03655dab58c7c039ac2
André Biseth <andre@biseth.net>
2022-06-17 18:36:57 +0200

    Bisand/issue17 (#20)

--- tests/test5-tibber-query.ts
+++ tests/test5-tibber-query.ts
@@ -1,20 +1,35 @@
 /* eslint-env mocha */
 import { TibberQuery } from '../src/index';
+import nock from 'nock';
 
-const config = {
-    apiEndpoint: { queryUrl: 'https://test.com', feedUrl: 'https://test.com', apiKey: '1337' },
-    active: false,
-};
+describe('Testing TibberQuery HTTP client', () => {
 
-const query = new TibberQuery(config);
+    beforeAll(() => { });
+    afterAll(() => { });
+    beforeEach(() => { });
 
-test('TibberQuery should be created', () => {
-    expect(query).toBeDefined();
-});
-test('TibberQuery Should be inactive', () => {
-    expect(query.active).toBe(false);
-});
-test('TibberQuery Should be inactive', () => {
-    query.active = true;
-    expect(query.active).toBe(true);
+    const tibberResponseHomes = { "viewer": { "homes": [{ "id": "c70dcbe5-4485-4821-933d-a8a86452737b", "address": { "address1": "Kungsgatan 8", "address2": null, "address3": null, "postalCode": "11759", "city": "Stockholm", "country": "SE", "latitude": "59.3362066", "longitude": "18.0675126" } }] } };
+
+    it('Should return a valid home', async () => {
+        const scope = nock('https://api.tibber.com')
+            .post('/')
+            .reply(200, tibberResponseHomes);
+        const query = new TibberQuery({ apiEndpoint: { apiKey: '1337', feedUrl: '', queryUrl: 'https://api.tibber.com' }, active: false });
+
+        const res = await query.getHomes();
+        expect(res).toMatchObject(tibberResponseHomes.viewer.homes);
+
+    }, 60000);
+
+    it('Should not crash when server returns an error', async () => {
+        const scope = nock('https://api.tibber.com')
+            .post('/')
+            .reply(502, 'Bad Gateway');
+        const query = new TibberQuery({ apiEndpoint: { apiKey: '1337', feedUrl: '', queryUrl: 'https://api.tibber.com' }, active: false });
+
+        expect(async () => {
+            const res = await query.getHomes();
+        }).rejects.toMatchObject({ httpCode: 502, responseMessage: 'Bad Gateway', statusCode: 502, statusMessage: null });
+
+    }, 60000);
 });
