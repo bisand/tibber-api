11446898e362affc2ee01f17de37303519a0b5bf
André Biseth <andre@biseth.net>
2022-12-29 11:24:03 +0000

    Added force exit to tests and bumped version

--- Version.ts
+++ Version.ts
@@ -1,9 +1,9 @@
-export const version = "5.0.3";
+export const version = "5.1.0-beta.0";
 export const versionMajor = 5;
-export const versionMinor = 0;
-export const versionPatch = 3;
-export const versionPrerelease = [];
-export const release = "5.0.3+20221228224349";
-export const gitSha = "f7d5d0ef7a8b35da57f2739349e7bc50079b550e";
-export const gitDate = new Date(1672267429000);
+export const versionMinor = 1;
+export const versionPatch = 0;
+export const versionPrerelease = ["beta",0];
+export const release = "5.1.0-beta.0+20221229112147";
+export const gitSha = "aff07fc05fda2ebbffa73189720f73eb33c7181c";
+export const gitDate = new Date(1672312907000);
 export default {version,versionMajor,versionMinor,versionPatch,versionPrerelease,release,gitSha,gitDate};
