0e5c588ea5617dd27258290d682a40333eda5561
André Biseth <andre@biseth.net>
2023-01-04 21:56:28 +0000

    Added missing env to run step

--- Version.ts
+++ Version.ts
@@ -1,9 +1,9 @@
-export const version = "5.1.0-beta.8";
+export const version = "5.1.0-beta.10";
 export const versionMajor = 5;
 export const versionMinor = 1;
 export const versionPatch = 0;
-export const versionPrerelease = ["beta",8];
-export const release = "5.1.0-beta.8+20230104205147";
-export const gitSha = "4b0d357879d345d7ddb749f7a943457dcf09992e";
-export const gitDate = new Date(1672865507000);
+export const versionPrerelease = ["beta",10];
+export const release = "5.1.0-beta.10+20230104215416";
+export const gitSha = "266c04583e55c84c5e8d8235a4c47ac87dfc84c7";
+export const gitDate = new Date(1672869256000);
 export default {version,versionMajor,versionMinor,versionPatch,versionPrerelease,release,gitSha,gitDate};
