a415ff3be275a4962b8b09ecaf61eee610083009
André Biseth <andre@biseth.net>
2023-01-04 21:30:22 +0000

    Added missing dot

--- Version.ts
+++ Version.ts
@@ -1,9 +1,9 @@
-export const version = "5.1.0-beta.7";
+export const version = "5.1.0-beta.8";
 export const versionMajor = 5;
 export const versionMinor = 1;
 export const versionPatch = 0;
-export const versionPrerelease = ["beta",7];
-export const release = "5.1.0-beta.7+20230104204422";
-export const gitSha = "6b20008cb113b35f1cd5106d96cc627943c8d358";
-export const gitDate = new Date(1672865062000);
+export const versionPrerelease = ["beta",8];
+export const release = "5.1.0-beta.8+20230104205147";
+export const gitSha = "4b0d357879d345d7ddb749f7a943457dcf09992e";
+export const gitDate = new Date(1672865507000);
 export default {version,versionMajor,versionMinor,versionPatch,versionPrerelease,release,gitSha,gitDate};
