44af155158c0b1889766ec738660d6e3d1847088
André Biseth <andre@biseth.net>
2023-01-04 20:50:37 +0000

    Fixed action bug

--- Version.ts
+++ Version.ts
@@ -1,9 +1,9 @@
-export const version = "5.1.0-beta.6";
+export const version = "5.1.0-beta.7";
 export const versionMajor = 5;
 export const versionMinor = 1;
 export const versionPatch = 0;
-export const versionPrerelease = ["beta",6];
-export const release = "5.1.0-beta.6+20230104184835";
-export const gitSha = "4efc1cc01b2fbefbe77ab5df2b13f93eb416260c";
-export const gitDate = new Date(1672858115000);
+export const versionPrerelease = ["beta",7];
+export const release = "5.1.0-beta.7+20230104204422";
+export const gitSha = "6b20008cb113b35f1cd5106d96cc627943c8d358";
+export const gitDate = new Date(1672865062000);
 export default {version,versionMajor,versionMinor,versionPatch,versionPrerelease,release,gitSha,gitDate};
