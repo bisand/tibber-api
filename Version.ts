1d7450d55f2dff5f8292ca64b606ab2321e612f8
André Biseth <andre@biseth.net>
2023-01-04 20:41:27 +0000

    Testing conditional action run

--- Version.ts
+++ Version.ts
@@ -1,9 +1,9 @@
-export const version = "5.1.0-beta.5";
+export const version = "5.1.0-beta.6";
 export const versionMajor = 5;
 export const versionMinor = 1;
 export const versionPatch = 0;
-export const versionPrerelease = ["beta",5];
-export const release = "5.1.0-beta.5+20230104183334";
-export const gitSha = "9f7a1b26174a98827e4c2081b8b5e2c2767c2107";
-export const gitDate = new Date(1672857214000);
+export const versionPrerelease = ["beta",6];
+export const release = "5.1.0-beta.6+20230104184835";
+export const gitSha = "4efc1cc01b2fbefbe77ab5df2b13f93eb416260c";
+export const gitDate = new Date(1672858115000);
 export default {version,versionMajor,versionMinor,versionPatch,versionPrerelease,release,gitSha,gitDate};
