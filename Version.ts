da8a1b6b896305b1e483d8a004dfab647e4d6b08
André Syvertsen Biseth <andre@biseth.net>
2023-07-07 20:17:13 +0000

    Updated version file

--- Version.ts
+++ Version.ts
@@ -1,9 +1,9 @@
-export const version = "5.1.8";
+export const version = "5.1.9";
 export const versionMajor = 5;
 export const versionMinor = 1;
-export const versionPatch = 8;
+export const versionPatch = 9;
 export const versionPrerelease = [];
-export const release = "5.1.8+20230320054708";
-export const gitSha = "8db845be8ff6435ff8e220782f6b5428806f7d69";
-export const gitDate = new Date(1679291228000);
+export const release = "5.1.9+20230707201655";
+export const gitSha = "05629a425532c42d8ee525f1a88783f95dc56fa3";
+export const gitDate = new Date(1688761015000);
 export default {version,versionMajor,versionMinor,versionPatch,versionPrerelease,release,gitSha,gitDate};
