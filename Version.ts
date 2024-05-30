f825fe4377afc1270e5071a52ce0e23bfc6e37d0
hombach <63002482+hombach@users.noreply.github.com>
2024-05-30 15:01:27 +0200

    Change version number to 5.1.10

--- Version.ts
+++ Version.ts
@@ -1,7 +1,7 @@
-export const version = "5.1.9";
+export const version = "5.1.10";
 export const versionMajor = 5;
 export const versionMinor = 1;
-export const versionPatch = 9;
+export const versionPatch = 10;
 export const versionPrerelease = [];
 export const release = "5.1.9+20230707201655";
 export const gitSha = "05629a425532c42d8ee525f1a88783f95dc56fa3";
