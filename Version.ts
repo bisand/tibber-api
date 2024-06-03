2c69bf428467791a7fa8847ace3aacd23626019f
André Biseth <andre@biseth.net>
2024-06-03 18:29:18 +0000

    Reverted changes

--- Version.ts
+++ Version.ts
@@ -1,9 +1,9 @@
-export const version = "5.1.10";
+export const version = "5.1.9";
 export const versionMajor = 5;
 export const versionMinor = 1;
-export const versionPatch = 10;
+export const versionPatch = 9;
 export const versionPrerelease = [];
-export const release = "5.1.10+20240603180808";
-export const gitSha = "870e5e18b90dcee44d3b49b1a3c623c445a8bd50";
-export const gitDate = new Date(1717438088000);
+export const release = "5.1.9+20240603182038";
+export const gitSha = "24e824db8b59bdb31240ae118ae6145846485049";
+export const gitDate = new Date(1717438838000);
 export default {version,versionMajor,versionMinor,versionPatch,versionPrerelease,release,gitSha,gitDate};
