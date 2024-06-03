24e824db8b59bdb31240ae118ae6145846485049
André Biseth <andre@biseth.net>
2024-06-03 18:20:38 +0000

    Updated packages and version

--- Version.ts
+++ Version.ts
@@ -1,9 +1,9 @@
-export const version = "5.1.9";
+export const version = "5.1.10";
 export const versionMajor = 5;
 export const versionMinor = 1;
-export const versionPatch = 9;
+export const versionPatch = 10;
 export const versionPrerelease = [];
-export const release = "5.1.9+20230707201655";
-export const gitSha = "05629a425532c42d8ee525f1a88783f95dc56fa3";
-export const gitDate = new Date(1688761015000);
+export const release = "5.1.10+20240603180808";
+export const gitSha = "870e5e18b90dcee44d3b49b1a3c623c445a8bd50";
+export const gitDate = new Date(1717438088000);
 export default {version,versionMajor,versionMinor,versionPatch,versionPrerelease,release,gitSha,gitDate};
