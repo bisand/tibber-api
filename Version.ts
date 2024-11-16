d971ae7b26dd189192c8394b1320b12d2bc92d50
André Biseth <andre@biseth.net>
2024-11-16 15:22:34 +0000

    Bumped version

--- Version.ts
+++ Version.ts
@@ -1,9 +1,9 @@
-export const version = "5.2.1";
+export const version = "5.3.0";
 export const versionMajor = 5;
-export const versionMinor = 2;
-export const versionPatch = 1;
+export const versionMinor = 3;
+export const versionPatch = 0;
 export const versionPrerelease = [];
-export const release = "5.2.1+20241112053854";
-export const gitSha = "70136b2bc5b239b1a13e2f5b2f08dd1d58afefcf";
-export const gitDate = new Date(1731389934000);
+export const release = "5.3.0+20241116152149";
+export const gitSha = "3b91be08e45a423f653d118a8f37164cc73cefbd";
+export const gitDate = new Date(1731770509000);
 export default {version,versionMajor,versionMinor,versionPatch,versionPrerelease,release,gitSha,gitDate};
