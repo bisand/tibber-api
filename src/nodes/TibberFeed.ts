cb4a786f68745f8ca59f27855d430b73e2179241
André <andre@biseth.net>
2025-04-29 20:20:26 +0000

    Remove automatic reconnection attempt on WebSocket close

--- src/nodes/TibberFeed.ts
+++ src/nodes/TibberFeed.ts
@@ -579,10 +579,6 @@ export class TibberFeed extends EventEmitter {
     private onWebSocketClose(event: WebSocket.CloseEvent) {
         this._isConnected = false;
         this.emit('disconnected', 'Disconnected from Tibber feed.');
-        // Immediately try to reconnect if still active
-        if (this._active && !this._isClosing) {
-            this.connectWithDelayWorker(this._retryBackoff);
-        }
     }
 
     /**
