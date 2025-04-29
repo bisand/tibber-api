8ac82f6cfabda490b502387064db4288c9076f0d
André <andre@biseth.net>
2025-04-29 18:31:08 +0000

    Bump version to 5.4.0-beta.5: update prerelease details and release information

--- Version.ts
+++ Version.ts
@@ -1,9 +1,9 @@
-export const version = "5.4.0-beta.4";
+export const version = "5.4.0-beta.5";
 export const versionMajor = 5;
 export const versionMinor = 4;
 export const versionPatch = 0;
-export const versionPrerelease = ["beta",4];
-export const release = "5.4.0-beta.4+20250429043746";
-export const gitSha = "3b3df92171137cf064feee141d1c09205397a47a";
-export const gitDate = new Date(1745901466000);
+export const versionPrerelease = ["beta",5];
+export const release = "5.4.0-beta.5+20250429183059";
+export const gitSha = "37c25b2cc8fbe6adbc64bf9d2f067feaba4e1ade";
+export const gitDate = new Date(1745951459000);
 export default {version,versionMajor,versionMinor,versionPatch,versionPrerelease,release,gitSha,gitDate};
