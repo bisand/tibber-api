5b87fd277df06936b191d9c787fdc1987aa70582
André Biseth <andre@biseth.net>
2025-05-19 12:37:52 +0000

    Bump version to 5.4.0: update version, release, and metadata

--- Version.ts
+++ Version.ts
@@ -1,9 +1,9 @@
-export const version = "5.4.0-beta.15";
+export const version = "5.4.0";
 export const versionMajor = 5;
 export const versionMinor = 4;
 export const versionPatch = 0;
-export const versionPrerelease = ["beta",15];
-export const release = "5.4.0-beta.15+20250515112008";
-export const gitSha = "1131b9b5e59e99cb33a3425b2714030673cbede0";
-export const gitDate = new Date(1747308008000);
+export const versionPrerelease = [];
+export const release = "5.4.0+20250519123728";
+export const gitSha = "75a06aac8a6a3c8f9a66fec52ab865313c8751ca";
+export const gitDate = new Date(1747658248000);
 export default {version,versionMajor,versionMinor,versionPatch,versionPrerelease,release,gitSha,gitDate};
