de7ac46aba9a0c6e446557770e011cd1560b998b
André <andre@biseth.net>
2025-10-02 19:54:39 +0000

    chore: update version to 5.5.1 and increment patch number

--- Version.ts
+++ Version.ts
@@ -1,11 +1,11 @@
-export const version = "5.5.0";
+export const version = "5.5.1";
 export const versionMajor = 5;
 export const versionMinor = 5;
-export const versionPatch = 0;
+export const versionPatch = 1;
 export const versionPrerelease = [];
-export const release = "5.5.0+20251002193546";
-export const gitSha = "b8011e633b7fa020394ceca95b43d118b0dbd2d4";
-export const gitDate = new Date(1759433746000);
+export const release = "5.5.1+20251002195339";
+export const gitSha = "e4bf1a227c5d5b87eba0ad514e8d9dfd46be6d45";
+export const gitDate = new Date(1759434819000);
 export default {
   version,
   versionMajor,
