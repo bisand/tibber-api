b8011e633b7fa020394ceca95b43d118b0dbd2d4
André <andre@biseth.net>
2025-10-02 19:35:46 +0000

    chore: update devDependencies to latest versions
    - Bump @jest/test-sequencer and @types/jest to version 30.x
    - Upgrade @typescript-eslint/eslint-plugin and parser to version 8.45.0
    - Update eslint to version 9.36.0
    - Upgrade jest to version 30.2.0
    - Update jsdoc-to-markdown to version 9.1.2
    - Upgrade mkver to version 4.3.0
    - Update nock to version 14.0.10
    - Upgrade prettier to version 3.6.2
    - Update ts-jest to version 29.4.4
    - Upgrade typescript to version 5.9.3

--- Version.ts
+++ Version.ts
@@ -1,9 +1,9 @@
-export const version = "5.4.2";
+export const version = "5.5.0";
 export const versionMajor = 5;
-export const versionMinor = 4;
-export const versionPatch = 2;
+export const versionMinor = 5;
+export const versionPatch = 0;
 export const versionPrerelease = [];
-export const release = "5.4.2+20250519131329";
-export const gitSha = "e1124260cbcc722d40d60ce3d3b1b09cf2726318";
-export const gitDate = new Date(1747660409000);
+export const release = "5.5.0+20251002192916";
+export const gitSha = "3ff6da790a652227ae9b6d70e0d285235dd3bacb";
+export const gitDate = new Date(1759433356000);
 export default {version,versionMajor,versionMinor,versionPatch,versionPrerelease,release,gitSha,gitDate};
